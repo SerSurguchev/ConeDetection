7a18e48fbb720cafa4655b45aa3fc9e3ced78936
SerSurguchev <71214107+SerSurguchev@users.noreply.github.com>
2022-07-04 21:46:43 +0300

    Update Cone_color_detect.py

--- Cone_color_detection/Cone_color_detect.py
+++ Cone_color_detection/Cone_color_detect.py
@@ -8,13 +8,15 @@ from PIL import Image, ImageEnhance, ImageStat, ImageTk
 import matplotlib.pyplot as plt
 import ast
 
+
 def calc_brightness(im_file):
     stat = ImageStat.Stat(im_file)
     r, g, b = stat.mean
     return math.sqrt(0.241 * (r ** 2) + 0.691 * (g ** 2) + 0.068 * (b ** 2))
 
-def point_into_rectangle(boxes, x, y):
 
+def point_into_rectangle(boxes, x, y):
+    
     '''
     :param boxes: (list of lists): Coordinates of all boxes on image
     :param x: (int): x point coordinate
@@ -43,8 +45,9 @@ def point_into_rectangle(boxes, x, y):
 
     return check
 
-def create_cone_rectangle(x, y, h, w, cone):
 
+def create_cone_rectangle(x, y, h, w, cone):
+    
     '''
     Сreate cone rectangle with black pixels around the edges
     :param x: (int): x point coordinate (left down)
@@ -67,10 +70,11 @@ def create_cone_rectangle(x, y, h, w, cone):
 
     return bitwise
 
-def get_bitwise(bitwise):
 
+def get_bitwise(bitwise):
+    
     '''
-    :param bitwise: (ndarray): Cone rectangle with black pixels around the edges
+    :param bitwise: (ndarray): Numpy image containing cone rectangle with black pixels around the edges
     :return: (ndarray): Cone rectangle with custom color space (red, yellow, blue)
     '''
 
@@ -94,33 +98,42 @@ def get_bitwise(bitwise):
 
     return bitwise
 
-def remove_white_naive(orig_bitwise, threshold):
 
-    height, width = orig_bitwise.shape[:2]
+def remove_white_naive(triangle, threshold):
+    
+    """
+    Removing very bright pixels, for example, pixels under sunlight reflection ect
+
+    :param triangle: (ndarray): Numpy image containing cone rectangle with black pixels around the edges
+    :param threshold: (int): The pixel above this threshold become zero
+    :return: (ndarray): Numpy array without very bright pixels
+    """
+    
+    height, width = triangle.shape[:2]
 
     for row in range(height):
         for elem in range(width):
-            pix_sum = int(np.sum(orig_bitwise[row][elem])/3)
-            orig_bitwise[row][elem] = np.where(pix_sum > threshold, 0, orig_bitwise[row][elem])
+            pix_sum = int(np.sum(triangle[row][elem]) / 3)
+            triangle[row][elem] = np.where(pix_sum > threshold, 0, triangle[row][elem])
 
-    return orig_bitwise
+    return triangle
+
 
 def brightness_change(grad_array):
-
     increase, decrease = [], []
 
     for i in range(1, len(grad_array)):
-        if grad_array[i-1] < grad_array[i]:
-            increase.append(grad_array[i] - grad_array[i-1])
+        if grad_array[i - 1] < grad_array[i]:
+            increase.append(grad_array[i] - grad_array[i - 1])
         else:
-            decrease.append(abs(grad_array[i] - grad_array[i-1]))
+            decrease.append(abs(grad_array[i] - grad_array[i - 1]))
 
     return sum(increase) >= sum(decrease)
 
-def check_cone_gradient(orig_bitwise):
 
+def check_cone_gradient(orig_bitwise):
     gray = cv2.cvtColor(orig_bitwise, cv2.COLOR_BGR2GRAY)
-    half = gray[round(gray.shape[0]/2):, :]
+    half = gray[round(gray.shape[0] / 2):, :]
     grad_array = np.zeros([half.shape[0]])
 
     for ind, row in enumerate(half):
@@ -128,11 +141,12 @@ def check_cone_gradient(orig_bitwise):
         mean = np.mean(lst, axis=0)
         grad_array[ind] = mean
 
-    return brightness_change(grad_array[:len(grad_array)//2])
+    return brightness_change(grad_array[:len(grad_array) // 2])
+
 
 def get_color(triangle, fallen, possible_big_cone):
-
-    '''
+    
+    """
     Detect cone class:
     0 black middle
     1 white middle
@@ -142,10 +156,10 @@ def get_color(triangle, fallen, possible_big_cone):
     :param fallen: (boolean): True if cone is fallen (box width greater than height)
     :param possible_big_cone: (boolean)
     :return: (int): cone class
-    '''
+    """
 
-    gray_bitwise = cv2.cvtColor(triangle, cv2.COLOR_BGR2GRAY)
-    half_b = triangle[round(triangle.shape[0]/2):, :]
+    half_b = triangle[round(triangle.shape[0] / 2):, :]
+    # gray_bitwise = cv2.cvtColor(triangle, cv2.COLOR_BGR2GRAY)
     # bitwise_center = gray_bitwise.item(round(gray_bitwise.shape[0] / 2), round(gray_bitwise.shape[1] / 2))
 
     b = (np.sum(half_b[:, :, 0]))
@@ -153,7 +167,7 @@ def get_color(triangle, fallen, possible_big_cone):
     or_sum = (b + r_g) / 100
 
     # Detect blue cone (first class)
-    if b > r_g and (np.sum(half_b[:, :, 0])) / (np.sum(half_b[:, :, 2])) > 0.7:
+    if b > r_g and np.sum(half_b[:, :, 0]) / np.sum(half_b[:, :, 2]) > 0.7:
         return 1
 
     # Detect yellow and red cone
@@ -164,38 +178,42 @@ def get_color(triangle, fallen, possible_big_cone):
 
         # Detect fallen cones
         if fallen:
-            return 2 if yr/yellow > 2 else 0
+            return 2 if yr / yellow > 2 else 0
 
         elif possible_big_cone and \
-                yr/yellow > 5:
+                yr / yellow > 5:
             return 2
 
         # Detect big orange cone (second unknown class)
         elif check_cone_gradient(orig_bitwise=triangle) and \
-                (yellow/yr < 0.7 or yr/yellow > 3):
+                (yellow / yr < 0.7 or yr / yellow > 3):
             return 2
 
         # Detect yellow cone (zero class)
         elif check_cone_gradient(orig_bitwise=triangle) and \
-                yellow/yr >= 0.7:
+                yellow / yr >= 0.7:
             return 0
 
         # Detect red cone
         else:
             return 1
 
-if __name__ == '__main__':
 
-    data_fr_file = pd.read_csv('/home/sergey/from_wind/programming/Computer_vision/all.csv')
-    dir = r'/home/sergey/from_wind/programming/Computer_vision/YOLO_Dataset/'
-    output_dir = r'/home/sergey/from_wind/programming/Computer_vision/dataset_grayscale/'
+def main(data_fr_file, dir, output_dir):
+    
+    """
+    Main Function
+    :param data_fr_file: (string): Path to csv file with bounding box coordinates
+    :param dir: (string): Path to images
+    :param output_dir: (string): Path where images and labels will be saved
+    """
 
     lens = len(glob.glob(dir + '*.jpg'))
 
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-    for i in range(1, lens):
+    for i in range(6102, lens):
 
         j = 5
         name_of_image = data_fr_file.iloc[i, 0]
@@ -203,16 +221,19 @@ if __name__ == '__main__':
         c_height, c_width = image.shape[:2]
         name_of_file = name_of_image[:-3] + "txt"
 
+        img_center = np.array(image.shape[1]) / 2
+        img_center = np.hstack((img_center, img_center)).astype('uint8')
+
         file1 = open(output_dir + 'gray_' + name_of_file, 'w')
 
-        # mask = pd.DataFrame(data_fr_file.iloc[i, j:]).dropna()
-        # bboxes = []
-        # for index, row in mask.iterrows():
-        #     h = ast.literal_eval(row[i])[2]
-        #     if h > 25:
-        #         bboxes.append(ast.literal_eval(row[i]))
-        #
-        # bboxes = sorted(bboxes, key=lambda x: x[2], reverse=True)
+        mask = pd.DataFrame(data_fr_file.iloc[i, j:]).dropna()
+        bounding_boxes = []
+        for index, row in mask.iterrows():
+            h = ast.literal_eval(row[i])[2]
+            if h > 25:
+                bounding_boxes.append(ast.literal_eval(row[i]))
+
+        bounding_boxes = np.asarray(bounding_boxes).astype('uint8')
 
         while True:
 
@@ -230,8 +251,8 @@ if __name__ == '__main__':
 
             if h > 25:
 
-                # check_point1 = point_into_rectangle(bboxes, x=x, y=y + h)
-                # check_point2 = point_into_rectangle(bboxes, x=x + w, y=y + h)
+                # check_point1 = point_into_rectangle(bounding_boxes, x=x, y=y + h)
+                # check_point2 = point_into_rectangle(bounding_boxes, x=x + w, y=y + h)
 
                 # if check_point1 or check_point2:
                 #     # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), 2)
@@ -273,14 +294,23 @@ if __name__ == '__main__':
                     # Write labels in .txt file
                     file1.write(f"{color_ind} {x_norm} {y_norm} {w_norm} {h_norm}\n")
 
-                    # cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,255), 2)
-                    # cv2.putText(image, str(color_ind), (cx, y - 5), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+                    # cv2.putText(image, str(color_ind), (round((x + x + w) / 2), y - 5),
+                    #             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
 
             j += 1
 
         file1.close()
         cv2.imwrite(output_dir + 'gray_' + name_of_image, cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
 
-    #     cv2.imshow('frame', image)
-    #     cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+#         cv2.imshow('frame', image)
+
+#         cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    data_fr_file = pd.read_csv('/home/sergey/from_wind/programming/Computer_vision/all.csv')
+    dir = r'/home/sergey/from_wind/programming/Computer_vision/YOLO_Dataset/'
+    output_dir = r'/home/sergey/from_wind/programming/Computer_vision/dataset_grayscale/'
+
+    main(data_fr_file, dir, output_dir)
