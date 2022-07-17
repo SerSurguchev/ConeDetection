688a5db8c68c97e0e92c02383687de4458cd1b59
SerSurguchev <71214107+SerSurguchev@users.noreply.github.com>
2022-07-17 16:38:27 +0300

    Update data_augmentation.py

--- Cone_color_detection/data_augmentation.py
+++ Cone_color_detection/data_augmentation.py
@@ -1,10 +1,14 @@
+import glob
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+import pandas as pd
 from box_utils import (draw_rect,
                        yolo_to_pascal_voc,
                        pascal_voc_to_yolo,
+                       from_yolo_to_dataframe,
                        clip_box)
+import os
+import shutil
 
 np.set_printoptions(suppress=True)
 
@@ -27,7 +31,7 @@ class Horizontalflip:
         (ndarray): Transfromed number of bounding boxes and 5 represents [label, x_min, y_min, x_max, y_max] of the box
         """
 
-        img_center = np.array(img.shape[:2])[::-1] / 2
+        img_center = np.array(image.shape[:2])[::-1] / 2
         img_center = np.hstack((img_center, img_center))
 
         image = image[:, ::-1, :]
@@ -97,8 +101,8 @@ class ScaleImage:
 
 class TranslateImage:
     """
-    Randomly translate image
-    
+    Translate image
+
     Bounding boxes which have an area of less than 50% in the remaining in the
     transformed image is dropped. The resolution is maintained.
 
@@ -159,3 +163,167 @@ class TranslateImage:
                                          bounding_boxes[:, :4]), axis=1)
 
         return image, bounding_boxes
+
+
+class ShearImage:
+    """
+    Shears an image in horizontal direction
+    :param shear_factor: (float): Factor by which the image is sheared in the y-direction
+    """
+
+    def __init__(self, shear_factor=0.1):
+        self.shear_factor = shear_factor
+
+    def __call__(self, image, bounding_boxes):
+        """
+        :param image: (ndarraay): Numpy image
+        :param bounding_boxes: (ndarray): Numpy array containing bounding boxes are represented in the
+        format [label, x_min, y_min, x_max, y_max]
+
+        returns: (ndarray): Scaled image in the numpy format
+        (ndarray): Transfromed number of bounding boxes and 5 represents [label, x_min, y_min, x_max, y_max] of the box
+        """
+
+        shear_factor = self.shear_factor
+
+        if shear_factor < 0:
+            image, bounding_boxes = Horizontalflip()(image, bounding_boxes)
+
+        matrix = np.array([
+            [1, np.abs(shear_factor), 0],
+            [0, 1, 0]
+        ])
+
+        nW = image.shape[1] \
+             + abs(shear_factor * image.shape[0])
+
+        bounding_boxes[:, [1, 3]] += ((bounding_boxes[:, [2, 4]]) * np.abs(shear_factor)).astype(int)
+
+        image = cv2.warpAffine(image, matrix, (int(nW), image.shape[0]))
+
+        return image, bounding_boxes
+
+
+def split(strng, sep, pos):
+    strng = strng.split(sep)
+    return sep.join(strng[:pos]), sep.join(strng[pos:])
+
+
+def main(images_path, output_dir):
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    for image_path in glob.glob(images_path + '*.jpg'):
+
+        print(image_path)
+
+        img = cv2.imread(image_path)
+        height, width = img.shape[:2]
+
+        path_to_im, im_name = split(image_path, '/', 7)
+
+        txt_file = image_path.split('.')[0] + '.txt'
+        path_to_txt, txt_name = split(txt_file, '/', 7)
+
+        boxes = np.loadtxt(txt_file, dtype=np.float64)
+
+        if len(boxes.shape) == 1:
+            boxes = np.array([boxes], dtype=np.float64)
+
+        pascal_voc_boxes = yolo_to_pascal_voc(boxes.copy(), height, width)
+        pascal_voc_boxes = pascal_voc_boxes[pascal_voc_boxes[:, 4].argsort()]
+
+        # -----------------------------------------------------------------------
+        # Scale image by various factor
+        for scale_x, scale_y in np.array([
+            [-0.15, -0.15],
+            [-0.1, -0.1],
+            [0.1, 0.1],
+            [0.15, 0.15],
+            [0.2, 0.2]
+        ]):
+            scaled_img, scaled_boxes = ScaleImage(scale_x=scale_x,
+                                                  scale_y=scale_y)(img.copy(),
+                                                                   pascal_voc_boxes.copy())
+
+            scaled_boxes = pascal_voc_to_yolo(scaled_boxes,
+                                              h_image=height, w_image=width)
+            df_scaled_boxes = from_yolo_to_dataframe(scaled_boxes)
+
+            # Save scaled images and their labels
+            cv2.imwrite(f'{output_dir}/scaled_to_{scale_x}_{im_name}',
+                        cv2.cvtColor(scaled_img, cv2.COLOR_BGR2GRAY))
+
+            with open(f'{output_dir}/scaled_to_{scale_x}_{txt_name}', 'a') as f:
+                df_as_string = df_scaled_boxes.to_string(header=False, index=False)
+                f.write(df_as_string)
+
+        # -----------------------------------------------------------------------
+        # Translate image by various factor
+        for translate_x, translate_y in np.array([
+            [0.1, 0.1],
+            [0.15, 0.15],
+            [0.2, 0.2]
+        ]):
+            translated_img, translated_box = TranslateImage(translate_x=translate_x,
+                                                            translate_y=translate_y)(img.copy(),
+                                                                                     pascal_voc_boxes.copy())
+
+            translated_box = pascal_voc_to_yolo(translated_box,
+                                                h_image=height, w_image=width)
+            df_translated_box = from_yolo_to_dataframe(translated_box)
+
+            # Save translated images and their labels
+            cv2.imwrite(f'{output_dir}/translated_to_{translate_x}_{im_name}',
+                        cv2.cvtColor(translated_img, cv2.COLOR_BGR2GRAY))
+
+            with open(f'{output_dir}/translated_to_{translate_x}_{txt_name}', 'a') as f:
+                df_as_string = df_translated_box.to_string(header=False, index=False)
+                f.write(df_as_string)
+
+        # -----------------------------------------------------------------------
+        # Shear image by various factor
+        for shear_factor in np.array([-0.2, -0.15, -0.1, 0.1, 0.15, 0.2]):
+            sheared_image, sheared_box = ShearImage(shear_factor=shear_factor)(img.copy(),
+                                                                               pascal_voc_boxes.copy())
+
+            sheared_box = pascal_voc_to_yolo(sheared_box,
+                                             h_image=height, w_image=width)
+
+            df_sheared_box = from_yolo_to_dataframe(sheared_box)
+
+            # Save translated images and their labels
+            cv2.imwrite(f'{output_dir}/sheared_to_{shear_factor}_{im_name}',
+                        cv2.cvtColor(sheared_image, cv2.COLOR_BGR2GRAY))
+
+            with open(f'{output_dir}/sheared_to_{shear_factor}_{txt_name}', 'a') as f:
+                df_as_string = df_sheared_box.to_string(header=False, index=False)
+                f.write(df_as_string)
+
+        # -----------------------------------------------------------------------
+        # Flip image
+        flipped_image, flipped_boxes = Horizontalflip()(img.copy(), pascal_voc_boxes.copy())
+
+        flipped_boxes = pascal_voc_to_yolo(flipped_boxes,
+                                           h_image=height,
+                                           w_image=width)
+
+        df_flipped_boxes = from_yolo_to_dataframe(flipped_boxes)
+
+        # Save flipped image and labels
+        cv2.imwrite(f'{output_dir}/flipped_{im_name}',
+                    cv2.cvtColor(flipped_image, cv2.COLOR_BGR2GRAY))
+
+        with open(f'{output_dir}/flipped_{txt_name}', 'a') as f:
+            df_as_string = df_flipped_boxes.to_string(header=False, index=False)
+            f.write(df_as_string)
+
+        # Final save original image as grayscale
+        cv2.imwrite(output_dir + '/' + im_name, cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+        shutil.copy2(txt_file, output_dir + '/' + txt_name)
+
+
+if __name__ == '__main__':
+    images_path = '/home/sergey/from_wind/programming/Computer_vision/correct_labels/'
+    output_dir = '/home/sergey/from_wind/programming/Computer_vision/correct_labels_final'
+    main(images_path=images_path, output_dir=output_dir)
